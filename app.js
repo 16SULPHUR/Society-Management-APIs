1fb8dd037535edb466436fbc944355689a07583c
“Ankit <22amtics124@gmail.com>
2024-01-11 15:14:28 +0530

    used simple cors()

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const cors = require("cors");
 const app = express();
 const port = 6969;
 
-app.use(
-  cors({
-    origin: "*",
-    methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
+//     credentials: true,
+//   })
+// );
+
+app.use(cors())
 
 const DB =
   "mongodb+srv://akpatil51340:%40Ankit2005@cluster0.rwylpqs.mongodb.net/SentosaEnclaveDataBase?retryWrites=true&w=majority";
