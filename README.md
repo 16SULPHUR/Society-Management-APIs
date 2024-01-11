e417f827e5447da33efca9adc513e3d99d2f08e2
“Ankit <22amtics124@gmail.com>
2024-01-11 13:17:36 +0530

    allowed cors to edit profile endpoint

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
-# > ***Society Management System API Endpoints***
+# ***Society Management System API Endpoints***
 
 > 1. https://smsapis.vercel.app/login  `POST`
 	Body: `{username: "username",password: "password"}`
 > 2. https://smsapis.vercel.app/allEvents 
 >  3. https://smsapis.vercel.app/allNotices
+>  4. https://smsapis.vercel.app/editProfile
--- controllers/_editProfileHandler.js
+++ controllers/_editProfileHandler.js
@@ -2,6 +2,13 @@ var fs = require("fs");
 var path = require("path");
 const User = require("../models/User");
 const uploadImage = require("./_uploadImage");
+const cors = require("cors")
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
 
 async function _editProfileHandler(req, res) {
   const body = req.body;
